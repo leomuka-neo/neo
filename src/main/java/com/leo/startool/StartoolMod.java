e29f0e53b85273d1e1a1a0b5d717b9beaceb8625
Leo <xleandros@proton.me>
2026-02-24 10:20:30 -0800

    test 3

--- src/main/java/com/leo/startool/StartoolMod.java
+++ src/main/java/com/leo/startool/StartoolMod.java
@@ -1,21 +1,11 @@
 package com.leo.startool;
 
+import com.leo.startool.item.ModItems;
+import net.minecraft.world.item.CreativeModeTabs;
 import org.slf4j.Logger;
 
 import com.mojang.logging.LogUtils;
 
-import net.minecraft.core.registries.BuiltInRegistries;
-import net.minecraft.core.registries.Registries;
-import net.minecraft.network.chat.Component;
-import net.minecraft.world.food.FoodProperties;
-import net.minecraft.world.item.BlockItem;
-import net.minecraft.world.item.CreativeModeTab;
-import net.minecraft.world.item.CreativeModeTabs;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.state.BlockBehaviour;
-import net.minecraft.world.level.material.MapColor;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.bus.api.SubscribeEvent;
@@ -24,15 +14,10 @@ import net.neoforged.fml.config.ModConfig;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
-import net.minecraft.client.Minecraft;
 import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.neoforged.neoforge.common.NeoForge;
 import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
 import net.neoforged.neoforge.event.server.ServerStartingEvent;
-import net.neoforged.neoforge.registries.DeferredBlock;
-import net.neoforged.neoforge.registries.DeferredHolder;
-import net.neoforged.neoforge.registries.DeferredItem;
-import net.neoforged.neoforge.registries.DeferredRegister;
 
 @Mod(StartoolMod.MOD_ID)
 public class StartoolMod {
@@ -41,6 +26,9 @@ public class StartoolMod {
     public StartoolMod(IEventBus modEventBus, ModContainer modContainer) {
         modEventBus.addListener(this::commonSetup);
         NeoForge.EVENT_BUS.register(this);
+
+        ModItems.register(modEventBus);
+
         modEventBus.addListener(this::addCreative);
         modContainer.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
     }
@@ -48,7 +36,9 @@ public class StartoolMod {
 
     }
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
-
+if(event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+    event.accept(ModItems.STARPICk);
+}
     }
     @SubscribeEvent
     public void onServerStarting(ServerStartingEvent event) {
--- src/main/java/com/leo/startool/item/ModItems.java
+++ src/main/java/com/leo/startool/item/ModItems.java
@@ -1,10 +1,24 @@
 package com.leo.startool.item;
 
 import com.leo.startool.StartoolMod;
+import net.minecraft.world.item.Item;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredItem;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
 public class ModItems {
     public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(StartoolMod.MOD_ID);
 
+
+    public static final DeferredItem<Item>STARPICk = ITEMS.register("starpick",
+            () -> new Item(new Item.Properties()));
+
+
+
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+
+    }
 
 }
