ff845ea7db27f46d8e339f9c7f17b2a3d18dae5d
Leo <xleandros@proton.me>
2026-02-24 09:37:07 -0800

    test 1

---
+++ src/main/java/com/leo/startool/item/ModItems.java
@@ -0,0 +1,10 @@
+package com.leo.startool.item;
+
+import com.leo.startool.StartoolMod;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+public class ModItems {
+    public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(StartoolMod.MODID);
+
+
+}
